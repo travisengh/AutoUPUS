f52e6f88bc05353bb234cbb3e4f967849cc4f45c
Travis Engh <engh.travis@outlook.com>
2021-11-12 21:13:24 -0600

    Update ubuntuUpdates.py

--- ubuntuUpdates.py
+++ ubuntuUpdates.py
@@ -1,7 +1,7 @@
 import os
 import datetime as dt
 
-pwd = '@ShadoW1234037%'
+pwd = 'mypasswd' #ENTER YOUT PASSWORD AS A STRING HERE!
 update = 'apt update -y && sudo apt upgrade -y'
 today = dt.datetime.today().day
 os.system('echo %s|sudo -S %s' % (pwd, update))
